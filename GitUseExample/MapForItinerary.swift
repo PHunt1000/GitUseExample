8198735ac8282a448cf1b9c72364e749a6964135
Peggy Hunt <peggy.hunt@d214.org>
2018-04-18 13:01:32 -0500

    Add new map-for-intinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Margaret A. Hunt on 4/18/18.
+//  Copyright © 2018 phunt1000. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
